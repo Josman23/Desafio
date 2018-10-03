abd8e633ba17c9a136862c644b2e2e4cfe68e2e8
Jose Manuel <josman_more23@hotmail.com>
2018-10-03 10:43:10 -0500

    Agregar proyecto Desafío

---
+++ src/Implementar.java
@@ -0,0 +1,15 @@
+import mx.com.sintelti.desafios.oporx.Solucion;
+
+public class Implementar implements Solucion {
+
+    @Override
+    public String convertir(String entrada) {
+
+        for(int i=0; i <= entrada.length(); i++){
+
+
+        }
+
+        return null;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+    public static void main(String[] args){
+        Implementar implementa= new Implementar();
+        String entrada="hola";
+        implementa.convertir(entrada);
+    }
+}
