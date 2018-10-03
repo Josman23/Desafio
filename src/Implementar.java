2c51904db03368a3d6cde95b471229688557ee16
Jose Manuel <josman_more23@hotmail.com>
2018-10-03 12:58:58 -0500

    Agregar contador de letras O e implementación de for a la clase Implementar.

--- src/Implementar.java
+++ src/Implementar.java
@@ -6,20 +6,26 @@ public class Implementar implements Solucion {
     @Override
     public String convertir(String entrada) {
 
-        String cadenaRetornada=null;
-        int posicionInicio=0;
+        String cadenaRetornada = null;
+        int posicionInicioSubstring = 0;
+        int totalDeLetrasO = 0;
 
         for(int i=1; i <= entrada.length(); i++){
 
-            String letraSustraida=entrada.substring(posicionInicio,i);
+            String letraSustraida=entrada.substring(posicionInicioSubstring,i);
 
             if(letraSustraida == "o" || letraSustraida == "O" ){
 
+                totalDeLetrasO ++;
+
+                for(int O=1; O <= totalDeLetrasO; O++ ){
+
+                }
             }else{
 
             }
 
-            posicionInicio ++;
+            posicionInicioSubstring ++;
         }
         return null;
     }
